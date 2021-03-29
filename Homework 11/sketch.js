298829bc7844e08123613273b39aa3c98a4f5204
SimonConkle <77299402+SimonConkle@users.noreply.github.com>
2021-03-29 15:35:42 -0600

    Hw

--- Homework 11/sketch.js
+++ Homework 11/sketch.js
@@ -1,52 +1,56 @@
-// x and y for my character
+// character
 var characterX = 100;
 var characterY = 100;
-// define the key codes for each letter
+
+// wasd
 var w = 87; 
 var s = 83;
 var a = 65;
 var d = 68;
 
-// x and y for a shape
+// shape 1
 var shapeX = 30;
 var shapeY = 50;
 var shapeXSpeed;
 var shapeYSpeed;
 
-// create a shape when the mouse is clicked
+// shape 2
+var shapeW = 60;
+var shapeZ = 80;
+var shapeWSpeed;
+var shapeZSpeed;
+
+// mouse click shape
 var mouseShapeX;
 var mouseShapeY;
 function setup()
 {
     createCanvas(500, 600);
-    // get a random speed when the it first starts
     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeWSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeZSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
 }
 
 function draw()
-{
-    background(120,45,78);
-    stroke(0);
-    fill(0);
+    {background(100,150,20);
     // top border
     rect(0,0,width,10);
     // left border
-    rect(0,0,10,height);
+    rect(0,0,10,height-50);
     // bottom border
     rect(0, height-10,width, 10);
     // right upper border
-    rect(width-10,0,10,height-50);
+    rect(width-10,0,10,height);
 
     // exit message
-    textSize(16);
-    text("EXIT", width-50,height-50)
+    textSize(25);
+    text("Exit", width-470,height-25)
 
-    //character
-    fill(23,40,123);
+    // character
+    fill(60,30,90);
     circle(characterX,characterY,25);
 
-    // handle the keys
     if(keyIsDown(w))
     {
         characterY -= 10;   
@@ -64,19 +68,22 @@ function draw()
         characterX += 10;   
     }
 
-    // potential enemy
-    fill(13,145,14);
-    // draw the shape
-    circle(shapeX, shapeY, 10);
+   
+    // enemy shapes
+    {circle(shapeX, shapeY, 10);
+        fill(50,25,10)
+    }
+    {circle(shapeW, shapeZ, 20);
+        fill(40,50,20)
+    }
 
-     // get a random speed when the it first starts
-     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-
-    // move the shape
+    // enemy movement 
     shapeX += shapeXSpeed;
     shapeY += shapeYSpeed;
-    // check to see if the shape has gone out of bounds
+    shapeW += shapeWSpeed;
+    shapeZ += shapeZSpeed;
+
+    // shape 1 out of bounds
     if(shapeX > width)
     {
         shapeX = 0;
@@ -94,18 +101,35 @@ function draw()
         shapeY = height;
     }
 
-    // check to see if the character has left the exit
-    if(characterX > width && characterY > width-50)
+    // shape 2 out of bounds
+    if(shapeW > width)
     {
-        fill(0);
+        shapeW = 0;
+    }
+    if(shapeW < 0)
+    {
+        shapeW = width;
+    }
+    if(shapeZ > height)
+    {
+        shapeZ = 0;
+    }
+    if(shapeZ < 0)
+    {
+        shapeZ = height;
+    }
+    // exit check
+    if(characterY > width && characterX < width-500)
+    {
         stroke(5);
-        textSize(26);
-        text("You Win!", width/2-50, height/2-50);
+        fill(0);
+        textSize(50);
+        text("You Win!", width/2-20, height/2-25);
     }
 
-    // create the shape based on the mouse click
-    fill(120,130,140);
-    circle(mouseShapeX, mouseShapeY, 25);
+    // mouse click
+    fill(100,100,100);
+    circle(mouseShapeX, mouseShapeY, 30);
 }
 
 function mouseClicked()
@@ -113,7 +137,7 @@ function mouseClicked()
     mouseShapeX = mouseX;
     mouseShapeY = mouseY;
 }
-/*
+
 function keyPressed() {
     if (keyCode === LEFT_ARROW) {
         characterX -= 10;
@@ -127,6 +151,4 @@ function keyPressed() {
     else if (keyCode === DOWN_ARROW) {
         characterY += 10;
     }
-
   }
-  */
